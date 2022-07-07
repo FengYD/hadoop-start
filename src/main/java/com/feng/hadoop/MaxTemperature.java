7ed94ba1e7d77464934d1221f2a879b83a541a61
fengyadong <f18813108509@gmail.com>
2022-07-07 22:11:54 +0800

    max temperature

---
+++ src/main/java/com/feng/hadoop/MaxTemperature.java
@@ -0,0 +1,42 @@
+package com.feng.hadoop;
+
+import com.feng.hadoop.mapper.MaxTemperatureMapper;
+import com.feng.hadoop.reducer.MaxTemperatureReducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+/**
+ * @author fengyadong
+ * @date 2022/7/7 22:01
+ * @description
+ */
+public class MaxTemperature {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(MaxTemperature.class);
+        job.setJobName("Max temperature");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 1 : 0);
+    }
+
+}
